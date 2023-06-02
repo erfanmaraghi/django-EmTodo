c6e715efda4173a57d1f46d9032565f209049174
Erfan <erfoonmaraghi@gmail.com>
2023-06-02 08:10:55 -0700

    NewTaskForm in views.py completed

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewTaskForm
 
 
 def index(request):
-    return render(request, "core/index.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.created_by = request.user
+            instance.save()
+            return redirect("core:index")
+    form = NewTaskForm()
+    return render(request, "core/index.html", {'form': form})
